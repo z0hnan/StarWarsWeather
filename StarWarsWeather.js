dfd476ec18eaf3aceb50ebe8b26338139e362952
z0hnan <simesim2006@gmail.com>
2023-02-04 20:27:17 +0100

    code works now, still no planets or images tho

---
+++ StarWarsWeather.js
@@ -0,0 +1,27 @@
+function APICall(){
+  fetch("http://api.weatherapi.com/v1/current.json?key=27c58c1cd520412b915144718230402&q=Randers&aqi=no")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.current.temp_c, "°C");
+    const TempText = document.getElementById("temp")
+    TempText.textContent = data.current.temp_c + "°C"
+    if(data.current.temp_c <= 0){
+      const Planet = document.getElementById("planet")
+      Planet.textContent = "Hoth"
+    }
+
+  })
+  .catch(error => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+}
+
+const interval = 60 * 60 * 1000;
+setInterval(APICall, interval);
+
+window.onload = APICall;
